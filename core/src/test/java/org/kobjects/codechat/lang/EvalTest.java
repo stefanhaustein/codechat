509e880995d247b64402fbf84d186b090835d815
stefan.haustein@gmail.com <stefan.haustein@gmail.com>
2017-06-02 23:32:36 +0200

    some simple unit tests

---
+++ core/src/test/java/org/kobjects/codechat/lang/EvalTest.java
@@ -0,0 +1,7 @@
+package org.kobjects.codechat.core;
+
+/**
+ * Created by haustein on 02.06.17.
+ */
+public class EvalTest {
+}
---
+++ core/src/test/java/org/kobjects/codechat/lang/TestShell.java
@@ -0,0 +1,7 @@
+package org.kobjects.codechat.lang;
+
+/**
+ * Created by haustein on 02.06.17.
+ */
+public class TestShell {
+}
