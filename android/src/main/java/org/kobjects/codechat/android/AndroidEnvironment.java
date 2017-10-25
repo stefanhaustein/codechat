fbc0a93bdc0dea33ff61b1ae2b0e6d598efe2420
stefan.haustein@gmail.com <stefan.haustein@gmail.com>
2017-10-25 23:51:24 +0200

    playsound abc support improvements

--- android/src/main/java/org/kobjects/codechat/android/AndroidEnvironment.java
+++ android/src/main/java/org/kobjects/codechat/android/AndroidEnvironment.java
@@ -36,36 +36,94 @@ public class AndroidEnvironment extends Environment implements Runnable {
     final Context context;
     private HashSet<String> sounds = new HashSet<String>();
 
-    void playSound(String s) {
-        try {
-            String name = Integer.toHexString(Character.codePointAt(s,0));
-            if (sounds.contains(name + ".mp3")) {
-                name += ".mp3";
-            } else if (sounds.contains(name + ".wav")) {
-                name += ".wav";
-            } else {
-                throw new RuntimeException("Sound '" + name + "' not found");
+    void playSound(final String s, int pos, boolean mayBlock) {
+        while (pos < s.length()) {
+            final int codePoint = Character.codePointAt(s, pos);
+            pos += Character.charCount(codePoint);
+            final double f;
+            switch (codePoint) {
+                case 'C': f = 261.63; break;
+                case 'D': f = 293.66; break;
+                case 'E': f = 329.63; break;
+                case 'F': f = 349.23; break;
+                case 'G': f = 392; break;
+                case 'A': f = 440; break;
+                case 'B': f = 493.88; break;
+                case 'c': f = 523.25; break;
+                case 'd': f = 587.33; break;
+                case 'e': f = 659.26; break;
+                case 'f': f = 698.46; break;
+                case 'g': f = 783.99; break;
+                case 'a': f = 880; break;
+                case 'b': f = 987.77; break;
+                default:
+                    try {
+                        String name = Integer.toHexString(codePoint);
+                        if (sounds.contains(name + ".mp3")) {
+                            name += ".mp3";
+                        } else if (sounds.contains(name + ".wav")) {
+                            name += ".wav";
+                        } else {
+                            continue;
+                        }
+                        AssetFileDescriptor descriptor = rootView.getContext().getAssets().openFd("sound/" + name);
+                        final MediaPlayer m = new MediaPlayer();
+                        m.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+                        descriptor.close();
+                        final int nextPos = pos;
+                        m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                            @Override
+                            public void onCompletion(MediaPlayer mediaPlayer) {
+                                m.release();
+                                playSound(s, nextPos, false);
+                            }
+                        });
+
+                        m.prepare();
+                        m.setVolume(1f, 1f);
+                        m.setLooping(false);
+                        m.start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return;
             }
-            AssetFileDescriptor descriptor = rootView.getContext().getAssets().openFd("sound/" +  name);
-            final MediaPlayer m = new MediaPlayer();
-            m.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
-            descriptor.close();
 
-            m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mediaPlayer) {
-                    m.release();
+            double duration = 0.25;
+            if (pos < s.length()) {
+                char c = s.charAt(pos);
+                boolean divide = false;
+                if (c == '/') {
+                    pos++;
+                    divide = true;
                 }
-            });
-
-            m.prepare();
-            m.setVolume(1f, 1f);
-            m.setLooping(false);
-            m.start();
-        } catch (IOException e) {
-            e.printStackTrace();
+                int len = 0;
+                while (pos < s.length() && s.charAt(pos) >= '0' && s.charAt(pos) <= '9') {
+                    len = len * 10 + (s.charAt(pos++) - '0');
+                }
+                if (divide) {
+                    duration /= len == 0 ? 2 : len;
+                } else if (len != 0) {
+                    duration *= len;
+                }
+            }
+            if (mayBlock) {
+                Tone tone = new Tone(f, duration);
+                tone.play();
+            } else {
+                final int finalPos = pos;
+                final double finalDuration = duration;
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Tone tone = new Tone(f, finalDuration);
+                        tone.play();
+                        playSound(s, finalPos, true);
+                    }
+                }).start();
+                return;
+            }
         }
-
     }
 
     public AndroidEnvironment(EnvironmentListener environmentListener, FrameLayout rootView, File codeDir) {
@@ -97,7 +155,7 @@ public class AndroidEnvironment extends Environment implements Runnable {
         addNativeFunction(new NativeFunction("playSound", Type.VOID,  "Plays the given sound", Type.STRING) {
             @Override
             protected Object eval(Object[] params) {
-                playSound((String) params[0]);
+                playSound((String) params[0], 0, false);
                 return null;
             }
         });
